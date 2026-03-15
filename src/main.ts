6d431a9a7b66ce25fb4103124e1c041f15c41d40
Pasiflora Dev <dev@pasiflora.local>
2026-03-15 15:53:44 +0000

    Enable HTTPS for NestJS server using SSL certs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const certPath = path.join(__dirname, '../../certs/fullchain.pem');
+  const keyPath = path.join(__dirname, '../../certs/privkey.pem');
+
+  const httpsOptions =
+    fs.existsSync(certPath) && fs.existsSync(keyPath)
+      ? {
+          cert: fs.readFileSync(certPath),
+          key: fs.readFileSync(keyPath),
+        }
+      : undefined;
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   await app.listen(3000);
 }
